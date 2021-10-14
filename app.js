442301f38247341f913b9a6bc38f41018b655467
Prince Verma <9914604404p@gmail.com>
2021-10-15 00:11:07 +0530

    Added navbar and styles

--- app.js
+++ app.js
@@ -4,13 +4,13 @@ const data = [
     "The thinlip mullet  is a species of fish in the family Mugilidae. It is found in shallow European waters and is a migratory species.  The thin lip mullet has an elongate body compressed laterally. The head is short and flattened and the mouth is broad with a narrow upper lip and no tubercles. There are two dorsal fins. It is steely blue above and paler beneath. The scales are large and there is no externally visible lateral line Its maximum length is around 70  cm, with the common specimen being around 35  cm. The largest specimens recorded weighed over three kilograms.Spawning takes place at sea, near the coast between September and February.",
   ],
   [
-    "Chicago Pile-1 (CP-1) was the world's first artificial nuclear reactor. On 2 December 1942, the first human-made self-sustaining nuclear chain reaction was initiated in CP-1, during an experiment led by Enrico Fermi. The secret development of the reactor was the first major technical achievement for the Manhattan Project, the Allied effort to create atomic bombs during World War II. Developed by the Metallurgical Laboratory at the University of Chicago, it was built under the west viewing stands of the original Stagg Field. Although the project's civilian and military leaders had misgivings about the possibility of a disastrous runaway reaction, they trusted Fermi's safety calculations and decided they could carry out the experiment in a densely populated area. Fermi described the reactor as a crude pile of black bricks and wooden timbers",
+    "Quantum technology is approaching the mainstream. Goldman Sachs recently announced that they could introduce quantum algorithms to price financial instruments in as soon as five years. Honeywell anticipates that quantum will form a $1 trillion industry in the decades ahead. But why are firms like Goldman taking this leap — especially with commercial quantum computers being possibly years away?",
   ],
   [
-    "The discovery of the neutron and its properties was central to the extraordinary developments in atomic physics in the first half of the 20th century. Early in the century, Ernest Rutherford developed a crude model of the atom, based on the gold foil experiment of Hans Geiger and Ernest Marsden. In this model, atoms had their mass and positive electric charge concentrated in a very small nucleus. By 1920 chemical isotopes had been discovered, the atomic masses had been determined to be (approximately) integer multiples of the mass of the hydrogen atom, and the atomic number had been identified as the charge on the nucleus. Throughout the 1920s, the nucleus was viewed as composed of combinations of protons and electrons, the two elementary particles known at the time, but that model presented several experimental and theoretical contradictions.",
+    "Dark matter is an invisible material that lurks throughout the cosmos. In fact, it makes up about 85 percent of the matter in the universe. Unlike the ordinary matter inside you, your computer, the planet and all the stars in the sky, dark matter doesn’t produce or reflect any light. For decades, physicists have tried to identify the particles that make up this mysterious substance. But so far, all searches have come up empty.",
   ],
   [
-    "Gliding is a recreational activity and competitive air sport[1] in which pilots fly unpowered aircraft known as gliders or sailplanes using naturally occurring currents of rising air in the atmosphere to remain airborne. The word soaring is also used for the sport. Gliding as a sport began in the 1920s. Initially the objective was to increase the duration of flights but soon pilots attempted cross-country flights away from the place of launch. Improvements in aerodynamics and in the understanding of weather phenomena have allowed greater distances at higher average speeds. Long distances are now flown using any of the main sources of rising air: ridge lift, thermals and lee waves. When conditions are favourable, experienced pilots can now fly hundreds of kilometres before returning to their home airfields; occasionally flights of more than 1,000 kilometres (621 mi) are achieved.",
+    "A crocodile cannot stick its tongue out.A shrimp's heart is in its head.It is physically impossible for pigs to look up into the sky.The 'sixth sick shei'ks sixth sheep'' is believed to be the toughest tongue twister in the English language.If you sneeze too hard, you could fracture a rib.Wearing headphones for just an hour could increase the bacteria in your ear by 700 times.In the course of an average lifetime, while sleeping you might eat around 70 assorted insects and 10 spiders, or more.Some lipsticks contain fish scales.Cat urine glows under a black-light.Like fingerprints, everyone's tongue print is different.Rubber bands last longer when refrigerated.There are 293 ways to make change for a dollar.The average person's left hand does 56% of the typing (when using the proper position of the hands on the keyboard; Hunting and pecking doesn't count!).A shark is the only known fish that can blink with both eyes.The longest one-syllable words in the English language are 'scraunche' and 'strengthed.' Some suggest that 'squirreled' could be included, but squirrel is intended to be pronounced as two syllables (squir-rel) according to most dictionaries. 'Screeched' and 'strengths' are two other long one-syllable words, but they only have 9 letters.'Dreamt' is the only English word that ends in the letters 'mt'.Almonds are a member of the peach family.Maine is the only state that has a one-syllable name.",
   ],
 ];
 
@@ -25,8 +25,9 @@ const words = text.split(" ");
 let slideCount = 1;
 let inputwords = [];
 let matches = 0;
-const resbtn=document.querySelector('#restart-button');
-
+const resbtn = document.querySelector("#restart-button");
+const topicList = document.querySelector(".topic-list");
+const topicSelector = document.querySelector("#topic-selector");
 /////////////////////////////////////////////////
 function findInputWords(inText) {
   // finding input words
@@ -71,36 +72,58 @@ input.addEventListener("input", inputEvent);
 let timeInterval;
 ////////////////////////////////timer
 const timertext = document.querySelector("#timerid");
-function setTimer(time=60){
- timeInterval = setInterval(() => {
-  timertext.innerText = `0:${time}`;
-  time--;
-  if (time <= 0) {
-    clearInterval(timeInterval);
-    timertext.style.fontSize = "0.6em";
-    timertext.innerText = `0:00-Time out`;
-    input.style.pointerEvents="none";
-    input.removeEventListener("input",inputEvent)
-    displayText.style.transform="translateY(0px)"
-    input.value="TIME UP!!\n"+` Words- ${matches}`
-    input.style.fontWeight="100";
-    input.style.textAlign="center";
-  }
-}, 1000);
+function setTimer(time = 60) {
+  input.removeAttribute("disabled");
+  timeInterval = setInterval(() => {
+    timertext.innerText = `0:${time}`;
+    time--;
+    if (time <= 0) {
+      clearInterval(timeInterval);
+      timertext.style.fontSize = "0.6em";
+      timertext.innerText = `0:00-Time out`;
+      input.style.pointerEvents = "none";
+      input.removeEventListener("input", inputEvent);
+      displayText.style.transform = "translateY(0px)";
+      input.value = "TIME UP!!\n" + ` Words- ${matches}`;
+      input.style.fontWeight = "100";
+      input.style.textAlign = "center";
+      input.setAttribute("disabled", "true");
+    }
+  }, 1000);
 }
-setTimer();
-resbtn.addEventListener('click',(e)=>{
-  input.value=""
-  displayText.style.transform="translateY(0px)"
-  displayText.innerHTML=initialhtml;
-  input.style.fontWeight="normal";
-  input.style.fontSize="1.5em";
-  input.style.textAlign="left";
+setTimer(60);
+resbtn.addEventListener("click", (e) => {
+  input.value = "";
+  displayText.style.transform = "translateY(0px)";
+  displayText.innerHTML = initialhtml;
   timertext.value = " ";
+  input.style.fontWeight = "normal";
+  input.style.fontSize = "1.3em";
+  input.style.textAlign = "left";
+  input.style.pointerEvents = "unset";
   timertext.style.fontSize = "1em";
 
-input.addEventListener("input", inputEvent);
+  input.addEventListener("input", inputEvent);
   clearInterval(timeInterval);
   setTimer(60);
+});
+let clicknum = 0;
+topicSelector.addEventListener("click", (e) => {
+  topicList.classList.toggle("display");
+  clicknum += 1;
+  if (clicknum > 1) {
+    console.log(e.target);
+    clicknum = 0;
+    if(e.target.value==-1){
+      let randomValue=Math.floor(Math.random()*data.length);
+    displayText.innerText=`${data[randomValue]}`
+    }else{
+    if(e.target.value==undefined){
+      displayText.innerText=data[0];
 
-})
+    }else{
+      displayText.innerText=`${data[e.target.value]}`
+    }
+    }
+  }
+});
